51c312492de59ba8f42bf981a1c0725c3d3e1ace
Diptesh95 <patildiptesh146@gmail.com>
2024-06-29 20:55:53 +0530

    Create Task-1 Solution.py

---
+++ Task-1 Solution.py
@@ -0,0 +1,48 @@
+#Creating a List 
+even_num = [2,4,6,8,10] 
+print("List", even_num)
+
+#Adding Element to the 'even_num' named List 
+even_num.append(14)
+
+#Removing Element from the 'even_num' named List 
+even_num.remove(2)
+
+#Modifying an Element in the 'even_num' named List 
+even_num[4] = 12
+
+# Printing a List 
+print("After updating a List :- ", even_num)
+
+
+
+#Creating a Set
+stu_info = {'Ram', 21, 'Male', 'Surat', 5.9} 
+print("Set:- ", stu_info)
+
+#Adding Element to the 'stu_info' named Set 
+stu_info.add('Hello')
+
+#Removing Element from the 'stu_info' Set 
+stu_info.discard(5.9)
+
+# Printing a Set 
+print("After updating a Set :- ", stu_info)
+
+
+
+#Creating a Dictionarie
+my_info = {'Name': 'Diptesh', 'Age': 18, 'City': 'Surat'} 
+print("Dictionarie:", my_info)
+
+#Adding Element to the 'my_info' named Dictionarie 
+my_info['Gender'] : 'Male'
+
+#Removing Element from the 'my_info' Dictionarie 
+del my_info['City']
+
+#Modifying an Element in the 'my_info' Dictionarie 
+my_info['Age']: 19
+
+# Printing a Dictionarie 
+print("After updating a Dictionarie :-", my_info)
